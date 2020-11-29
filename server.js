5896e3c62f2d1797a2c23d3ca3c0abb49d2ec6d8
olawale micheal juwon <olawalejuwon@gmail.com>
2020-11-29 23:14:06 +0100

    done

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', function (req, res) {
- res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
 app.listen(PORT);
